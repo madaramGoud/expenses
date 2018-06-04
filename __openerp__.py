1dab1a26da4b2b15a134aeffe005c551f03bc5ad
madaramGoud <37170927+madaramGoud@users.noreply.github.com>
2018-06-04 16:56:13 +0530

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -3,7 +3,7 @@
 	'version' : '0.1',
 	'category' : 'expense',
 	'description' : """
-		This module is created to handle Expenses in Nowfloats
+		This module is created to handle Expenses
 	""",
     'author': 'OpenErp4you',
     'depends': ['base_setup','hr','hr_expense', 'hr_contract','mail'],
